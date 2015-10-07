ceccd55a2d09237c8efda3dae946adc6e87c71be
ble <ble@lesfurets.com>
2015-10-07 17:34:49 +0200

    Anagram randori - Session 1

--- anagram/src/main/java/com/lesfurets/anagram/AnagramJava7.java
+++ anagram/src/main/java/com/lesfurets/anagram/AnagramJava7.java
@@ -3,11 +3,28 @@
  */
 package com.lesfurets.anagram;
 
+import java.util.HashSet;
 import java.util.Set;
+
+import static java.util.Collections.emptySet;
 
 public class AnagramJava7 implements Anagram {
     @Override
     public Set<String> anagrams(String word, Set<String> candidates) {
-        return null;
+        if (word == null || candidates == null) {
+            return emptySet();
+        }
+
+        Set<String> result = new HashSet<>();
+        for (String candidate : candidates) {
+            if (isAnagram(word, candidate)) {
+                result.add(candidate);
+            }
+        }
+        return result;
+    }
+
+    private boolean isAnagram(String word, String candidate) {
+        return word.length()==candidate.length();
     }
 }
--- anagram/src/test/java/com/lesfurets/anagram/AnagramJava7Test.java
+++ anagram/src/test/java/com/lesfurets/anagram/AnagramJava7Test.java
@@ -2,6 +2,7 @@ package com.lesfurets.anagram;
 
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -23,6 +24,37 @@ public class AnagramJava7Test {
 
         // Then
         assertThat(anagrams).isEmpty();
+    }
+
+    @Test
+    public void should_return_itself() {
+        String word = "lol";
+        Set<String> candidate = new HashSet<>(Arrays.asList(word));
+        assertThat(anagram.anagrams(word, candidate)).containsExactly(word);
+    }
+
+    @Test
+    public void should_return_emptyset_for_word_null() {
+        assertThat(anagram.anagrams(null, new HashSet<String>())).isEmpty();
+    }
+
+    @Test
+    public void should_return_emptyset_for_candidates_null() {
+        assertThat(anagram.anagrams("", null)).isEmpty();
+    }
+
+    @Test
+    public void should_return_emptyset_when_candidates_doesnt_contain_anagram() {
+        String word = "lol";
+        Set<String> candidate = new HashSet<>(Arrays.asList("ooisua"));
+        assertThat(anagram.anagrams(word, candidate)).isEmpty();
+    }
+
+    @Test
+    public void should_find_anagram_of_word() {
+        String word = "ab";
+        Set<String> candidate = new HashSet<>(Arrays.asList("ba"));
+        assertThat(anagram.anagrams(word, candidate)).containsExactly("ba");
     }
 
 }
