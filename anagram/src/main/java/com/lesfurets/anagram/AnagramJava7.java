9a1b72952c8701a8aef3860db440535d58833d76
bde <bde@lesfurets.com>
2015-10-16 17:06:20 +0200

    Anagram randori - session 3

--- anagram/src/main/java/com/lesfurets/anagram/AnagramJava7.java
+++ anagram/src/main/java/com/lesfurets/anagram/AnagramJava7.java
@@ -3,11 +3,38 @@
  */
 package com.lesfurets.anagram;
 
-import java.util.Set;
+import java.util.*;
 
 public class AnagramJava7 implements Anagram {
+
+
     @Override
-    public Set<String> anagrams(String word, Set<String> candidates) {
-        return null;
+    public Set<String> anagrams(final String word, Set<String> candidates) {
+        if (word == null || word.isEmpty()) {
+            throw new IllegalArgumentException("Word is null or empty!");
+        }
+
+        Set<String> result = new HashSet<>();
+        Map<Character, Integer> wordCharsFrequency = toCharsFrequency(word);
+
+        for (String candidate : candidates) {
+            final Map<Character, Integer> candidateCharsFrequency = toCharsFrequency(candidate);
+            if (wordCharsFrequency.equals(candidateCharsFrequency) && !word.equals(candidate)) {
+                result.add(candidate);
+            }
+        }
+
+        return result;
+    }
+
+    private Map<Character, Integer> toCharsFrequency(String word) {
+        Map<Character, Integer> charsFrequency = new HashMap<>();
+        for (char c : word.toCharArray()) {
+            if (!charsFrequency.containsKey(c)) {
+                charsFrequency.put(c, 0);
+            }
+            charsFrequency.put(c, charsFrequency.get(c) + 1);
+        }
+        return charsFrequency;
     }
 }
--- anagram/src/test/java/com/lesfurets/anagram/AnagramJava7Test.java
+++ anagram/src/test/java/com/lesfurets/anagram/AnagramJava7Test.java
@@ -1,12 +1,12 @@
 package com.lesfurets.anagram;
 
-import org.junit.Test;
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.HashSet;
 import java.util.Set;
 
-import static java.util.Arrays.asList;
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Test;
 
 public class AnagramJava7Test {
 
@@ -25,4 +25,50 @@ public class AnagramJava7Test {
         assertThat(anagrams).isEmpty();
     }
 
+
+    @Test(expected = IllegalArgumentException.class)
+    public void should_return_error_when_word_is_null() {
+        // Given
+        String word = null;
+        Set<String> candidates = new HashSet<>(asList("hello", "world", "zombies", "pants"));
+
+        // When
+       anagram.anagrams(word, candidates);
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void should_return_error_when_word_is_empty() {
+        // Given
+        String word = "";
+        Set<String> candidates = new HashSet<>(asList("hello", "world", "zombies", "pants"));
+
+        // When
+         anagram.anagrams(word, candidates);
+    }
+
+    @Test
+    public void should_return_anagram_when_anagram_present() throws Exception {
+        // Given
+        String word = "diaper";
+        Set<String> candidates = new HashSet<>(asList("hello","perdia", "world", "zombies", "pants"));
+
+        // When
+        Set<String> anagrams = anagram.anagrams(word, candidates);
+
+        // Then
+        assertThat(anagrams).containsOnly("perdia");
+    }
+
+    @Test
+    public void should_not_return_the_word_itself() throws Exception {
+        String word = "diaper";
+        Set<String> candidates = new HashSet<>(asList("hello","diaper", "world", "zombies", "pants"));
+
+        // When
+        Set<String> anagrams = anagram.anagrams(word, candidates);
+
+        // Then
+        assertThat(anagrams).isEmpty();
+    }
 }
