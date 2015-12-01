5317af1b95a3da74b111e1cfaec62035f5c2497c
Mathieu Bolla <mathieu.bolla@gmail.com>
2015-12-01 09:21:06 +0100

    Validation de la dernière question d'exercice

--- structures-de-donnees/src/main/java/QuoteService.java
+++ structures-de-donnees/src/main/java/QuoteService.java
@@ -2,4 +2,11 @@ import java.util.*;
 
 public class QuoteService implements QuoteProcessor {
 
+    @Override
+    public SortedSet<Quote> getQuotesWithDistinctEmailsSortedInReversAlphaFrom(Iterable<Quote> quotes, String
+                    fromEmail) {
+        TreeSet<Quote> quoteTreeSet = new TreeSet<>(Comparator.comparing(Quote::getEmail).reversed());
+        quotes.forEach(quoteTreeSet::add);
+        return quoteTreeSet.headSet(new Quote(null, fromEmail, null), false);
+    }
 }
--- structures-de-donnees/src/test/java/QuoteProcessorTest.java
+++ structures-de-donnees/src/test/java/QuoteProcessorTest.java
@@ -1,10 +1,27 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.SortedSet;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class QuoteProcessorTest {
 
+    @Test
+    public void should_send_reverse_alpha_ordered_filtered_from_email_exclusive_set() {
+        String excludedEmail = "b@b.com";
+        Quote quoteExcluded = Quote.from("2014/11/23;a@a.com;provider");
+        Quote quoteIncludedAsEqual = Quote.from("2014/11/23;" + excludedEmail + ";provider");
+        Quote quoteIncluded = Quote.from("2014/11/23;c@c.com;provider");
+
+        QuoteProcessor quoteProcessor = new QuoteService();
+
+        SortedSet<Quote> quotesWithDistinctEmailsSortedInReversAlphaFrom = quoteProcessor
+                        .getQuotesWithDistinctEmailsSortedInReversAlphaFrom(Arrays.asList(quoteExcluded,
+                                        quoteIncludedAsEqual, quoteIncluded), excludedEmail);
+
+        assertThat(quotesWithDistinctEmailsSortedInReversAlphaFrom).containsExactly(
+                        quoteIncluded
+        );
+    }
 }
