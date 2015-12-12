115a42e38a1cd77a480bdfbf7996e3207fa1f2c7
Bastien LEMALE <blemale@excilys.com>
2015-12-12 10:24:11 +0100

    A better solution

--- structures-de-donnees/src/main/java/QuoteService.java
+++ structures-de-donnees/src/main/java/QuoteService.java
@@ -1,9 +1,15 @@
-import java.util.*;
-import java.util.stream.StreamSupport;
-
 import static java.util.Comparator.comparing;
-import static java.util.function.BinaryOperator.maxBy;
-import static java.util.stream.Collectors.*;
+import static java.util.stream.Collectors.toCollection;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public class QuoteService implements QuoteProcessor {
 
@@ -22,19 +28,47 @@ public class QuoteService implements QuoteProcessor {
 
     @Override
     public Set<Quote> getDistinctQuoteByEmail(Iterable<Quote> quotes) {
-
-        return StreamSupport.stream(quotes.spliterator(), false)
-                .collect(groupingBy(Quote::getEmail, reducing(maxBy(comparing(Quote::getDate)))))
-                .values().stream()
-                .filter(Optional::isPresent)
-                .map(Optional::get)
+        return distinctByEmail(quotes)
                 .collect(toSet());
     }
 
     @Override
     public SortedSet<Quote> getQuotesWithDistinctEmailSortedInReversAlpha(Iterable<Quote> quotes) {
-        SortedSet<Quote> result = new TreeSet<>(comparing(Quote::getEmail).reversed());
-        result.addAll(getDistinctQuoteByEmail(quotes));
-        return result;
+        Supplier<TreeSet<Quote>> treeSetSupplier =
+            () -> new TreeSet<>(comparing(Quote::getEmail).reversed());
+        return distinctByEmail(quotes)
+            .collect(toCollection(treeSetSupplier));
+    }
+
+    private Stream<Quote> distinctByEmail(Iterable<Quote> quotes) {
+        return StreamSupport.stream(quotes.spliterator(), false)
+            .map(EMailEqualityQuote::new)
+            .distinct()
+            .map(EMailEqualityQuote::quote);
+    }
+
+    private static class EMailEqualityQuote {
+        private final Quote quote;
+
+        public EMailEqualityQuote(Quote quote) {
+            this.quote = quote;
+        }
+
+        public Quote quote() {
+            return quote;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(quote.getEmail());
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {return true;}
+            if (obj == null || getClass() != obj.getClass()) {return false;}
+            final EMailEqualityQuote other = (EMailEqualityQuote) obj;
+            return Objects.equals(this.quote.getEmail(), other.quote.getEmail());
+        }
     }
 }
