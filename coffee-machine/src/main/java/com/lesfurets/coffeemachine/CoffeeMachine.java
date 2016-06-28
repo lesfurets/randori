b1bb3d2c4307b839ce82aa612aab708c39adf7ad
hvi <hvi@lesfurets.com>
2016-06-28 18:07:53 +0200

    refacto : inline output

--- coffee-machine/src/main/java/com/lesfurets/coffeemachine/CoffeeMachine.java
+++ coffee-machine/src/main/java/com/lesfurets/coffeemachine/CoffeeMachine.java
@@ -5,13 +5,8 @@ import com.lesfurets.coffeemachine.model.EDrink;
 class CoffeeMachine {
 
     static String makeCoffee(EDrink drink, int nbSugar) {
-        String output = drink.getCode();
-        output += ":";
         String sugar = nbSugar == 0 ? "" : String.valueOf(nbSugar);
-        output += sugar;
-        output += ":";
         String stick = nbSugar == 0 ? "" : "0";
-        output += stick;
-        return output;
+        return drink.getCode() + ":" + sugar + ":" + stick;
     }
 }
