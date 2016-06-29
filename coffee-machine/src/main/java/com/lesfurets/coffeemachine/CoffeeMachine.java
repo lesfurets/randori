1b2b7b1b9b9abfd96a673d2d0cf6059e281c3bcc
hvi <hvi@lesfurets.com>
2016-06-29 10:39:20 +0200

    refacto

--- coffee-machine/src/main/java/com/lesfurets/coffeemachine/CoffeeMachine.java
+++ coffee-machine/src/main/java/com/lesfurets/coffeemachine/CoffeeMachine.java
@@ -4,37 +4,9 @@ import com.lesfurets.coffeemachine.model.EDrink;
 
 class CoffeeMachine {
 
-    private EDrink drink;
-
-    static String makeCoffee(EDrink drink, int sugar) {
-        String output = "";
-        switch (drink) {
-            case COFFEE:
-                output += EDrink.COFFEE.getCode();
-                break;
-            case TEA:
-                output += EDrink.TEA.getCode();
-                break;
-            case CHOCOLATE:
-                output += EDrink.CHOCOLATE.getCode();
-                break;
-        }
-        output += ":";
-        if (sugar >= 1) {
-            output += sugar;
-        }
-        output += ":";
-        if (sugar >= 1) {
-            output += "0";
-        }
-        return output;
-    }
-
-    public EDrink getDrink() {
-        return drink;
-    }
-
-    public void setDrink(EDrink drink) {
-        this.drink = drink;
+    static String makeCoffee(EDrink drink, int nbSugar) {
+        String sugar = nbSugar == 0 ? "" : String.valueOf(nbSugar);
+        String stick = nbSugar == 0 ? "" : "0";
+        return drink.getCode() + ":" + sugar + ":" + stick;
     }
 }
