fe796e477a031b07f6f52e3985c2327afe6bc1c2
hvi <hvi@lesfurets.com>
2016-07-01 16:10:01 +0200

    code cleanup

--- coffee-machine/src/main/java/com/lesfurets/coffeemachine/CoffeeMachine.java
+++ coffee-machine/src/main/java/com/lesfurets/coffeemachine/CoffeeMachine.java
@@ -4,8 +4,11 @@ import com.lesfurets.coffeemachine.model.EDrink;
 
 class CoffeeMachine {
 
+    private static final String SEPARATOR = ":";
+    private static final String STICK = ":0";
+
     static String makeCoffee(EDrink drink, int sugar) {
-        return drink.getCode() + ":" + (sugar > 0 ? sugar + ":0" : ":");
+        return drink.getCode() + SEPARATOR + (sugar > 0 ? sugar + STICK : SEPARATOR);
     }
 
 }
