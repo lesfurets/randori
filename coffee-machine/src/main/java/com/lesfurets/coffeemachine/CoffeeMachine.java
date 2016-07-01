28dee900f341ac70816728b584baabb0d8287bda
hvi <hvi@lesfurets.com>
2016-07-01 16:07:06 +0200

    commit 2

--- coffee-machine/src/main/java/com/lesfurets/coffeemachine/CoffeeMachine.java
+++ coffee-machine/src/main/java/com/lesfurets/coffeemachine/CoffeeMachine.java
@@ -7,6 +7,7 @@ class CoffeeMachine {
     private static final String SEPARATOR = ":";
     private static final String STICK = ":0";
 
+//commit2
     static String makeCoffee(EDrink drink, int sugar) {
         return drink.getCode() + SEPARATOR + (sugar > 0 ? sugar + STICK : SEPARATOR);
     }
