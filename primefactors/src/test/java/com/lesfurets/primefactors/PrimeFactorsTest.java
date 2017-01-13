fe4a762d7e0b2b1c597c9fac632a80fbee7303a1
hvi <hvi@lesfurets.com>
2017-01-13 16:27:07 +0100

    Add test negative number

--- primefactors/src/test/java/com/lesfurets/primefactors/PrimeFactorsTest.java
+++ primefactors/src/test/java/com/lesfurets/primefactors/PrimeFactorsTest.java
@@ -1,5 +1,13 @@
 package com.lesfurets.primefactors;
 
+import org.junit.Test;
+
 public class PrimeFactorsTest {
 
+    private PrimeFactors primeFactors = new PrimeFactors();
+
+    @Test(expected = IllegalArgumentException.class)
+    public void should_throw_IllegalArgument_if_given_negative_number() throws Exception {
+        primeFactors.compute(-1);
+    }
 }
