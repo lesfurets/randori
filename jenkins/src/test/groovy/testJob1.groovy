e8559dd45bd4c25e455b502e722cf610782497da
hvi <hvi@lesfurets.com>
2017-01-27 16:42:35 +0100

    first commit

--- jenkins/src/test/groovy/testJob1.groovy
+++ jenkins/src/test/groovy/testJob1.groovy
@@ -1,6 +1,7 @@
 package com.lesfurets.jenkins
 
 import com.lesfurets.jenkins.helpers.BaseRegressionTest
+import org.assertj.core.api.Assertions
 import org.junit.Before
 import org.junit.Test
 
@@ -17,11 +18,23 @@ class TestJobRandoriPipeline extends BaseRegressionTest {
 
     @Test
     void testRandori() throws Exception {
+        helper.registerAllowedMethod('sh', [Map.class], {"origin/hvinet\norigin/older_branch"})
         def scriptLoader = loadScript('lib/scriptLoader.jenkins')
         def script = scriptLoader.configure('job/randoriPipeline.jenkins')
         script.execute()
         printCallStack()
         super.testNonRegression(false)
+    }
+
+    @Test
+    void getBranchList_should_map_to_Branch() throws Exception {
+        helper.registerAllowedMethod('sh', [Map.class], {"origin/hvinet\norigin/older_branch"})
+        def scriptLoader = loadScript('lib/scriptLoader.jenkins')
+        def script = scriptLoader.configure('job/randoriPipeline.jenkins')
+
+        List list = script.getBranchList("origin/hvinet\n" +
+                "origin/older_branch")
+        Assertions.assertThat(list).hasSize(2)
     }
 
     @Test(expected = Exception.class)
